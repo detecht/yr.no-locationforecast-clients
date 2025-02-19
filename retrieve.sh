65a97eaa41720271a4ad08a200aedf074e2170fe
Magnus Spånggård <magnus@detecht.se>
2025-02-19 13:01:55 +0100

    removed weather type patch since yr has fixed this in their swagger file.

--- retrieve.sh
+++ retrieve.sh
@@ -7,7 +7,4 @@ wget -O swagger.json https://api.met.no/weatherapi/locationforecast/2.0/swagger
 echo "Adding production host to swagger definition"
 jq -r '.host = "api.met.no"' swagger.json > swagger.json.tmp && mv swagger.json.tmp swagger.json
 
-echo "Patching WeatherSymbol defention to allow propper enum code generation."
-git apply weather_symbol.patch
-
 echo "Done!"
