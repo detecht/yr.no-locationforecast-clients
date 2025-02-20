c5cad49e2cd960e99aa2bf5f31d38739cf302231
Magnus Spånggård <magnus@detecht.se>
2025-02-20 12:48:59 +0100

    updated retrieve better handle errors.

--- generate.sh
+++ generate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-npx @openapitools/openapi-generator-cli generate
+# Check if npm is installed
+if ! command -v npm &> /dev/null; then
+    echo "npm could not be found. Please install it."
+    exit 1
+fi
 
-pushd clients/node/yr.no-locationforecast
-npm install
-popd
+# Generate OpenAPI client code using openapi-generator-cli
+echo "Generating OpenAPI client code"
+if ! npx @openapitools/openapi-generator-cli generate; then
+    echo "Failed to generate OpenAPI client code. Aborting."
+    exit 1
+fi
--- retrieve.sh
+++ retrieve.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-echo "Fetching swagger defenition from met.no"
-wget -O swagger.json https://api.met.no/weatherapi/locationforecast/2.0/swagger
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq could not be found. Please install it."
+    exit 1
+fi
 
+# Check if wget is installed
+if ! command -v wget &> /dev/null; then
+    echo "wget could not be found. Please install it."
+    exit 1
+fi
 
-echo "Adding production host to swagger definition"
-jq -r '.host = "api.met.no"' swagger.json > swagger.json.tmp && mv swagger.json.tmp swagger.json
+# Fetching Swagger definition from met.no
+echo "Fetching Swagger definition from met.no"
+if ! wget -O swagger.json https://api.met.no/weatherapi/locationforecast/2.0/swagger; then
+    echo "Failed to download the Swagger file. Aborting."
+    exit 1
+fi
+
+# Adding production host to Swagger definition
+echo "Adding production host to Swagger definition"
+if ! jq -r '.host = "api.met.no"' swagger.json > swagger.json.tmp; then
+    rm swagger.json.tmp
+    echo "Failed to update the Swagger file. Aborting."
+    exit 1
+fi
+mv swagger.json.tmp swagger.json
 
 echo "Done!"
